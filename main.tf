ecbd80e47a6b6f4d35a343f1d90d845284e14d19
TranDanCoSung <112317781+nhokhoa0908@users.noreply.github.com>
2023-12-21 13:19:01 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,15 +17,26 @@ resource "aws_vpc" "example" {
   }
 }
 
-# Create a public subnet
-resource "aws_subnet" "public_subnet" {
+# Create a public subnet 1
+resource "aws_subnet" "public_subnet_1" {
   vpc_id     = aws_vpc.example.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = var.aws_availability_zone
-  map_public_ip_on_launch = true
+  availability_zone = "ap-southeast-1a"
 
   tags = { 
-    Name = "public-subnet"
+    Name = "public-subnet-1"
+    env  = "dev"
+  }
+}
+
+# Create a public subnet 2
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-southeast-1b"
+
+  tags = { 
+    Name = "public-subnet-2"
     env  = "dev"
   }
 }
@@ -34,7 +45,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.example.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = var.aws_availability_zone
+  availability_zone = "ap-southeast-1c"
 
   tags = { 
     Name = "private-subnet"
@@ -42,8 +53,34 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+# Create EC2 instances in public subnet 1
+resource "aws_instance" "ec2_instance_public_1" {
+  count         = 2
+  ami           = "ami-02453f5468b897e31"  
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "ec2-instance-public-1-${count.index + 1}"
+    env  = "dev"
+  }
+}
+
+# Create EC2 instances in public subnet 2
+resource "aws_instance" "ec2_instance_public_2" {
+  count         = 2
+  ami           = "ami-02453f5468b897e31"  
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet_2.id
+
+  tags = {
+    Name = "ec2-instance-public-2-${count.index + 1}"
+    env  = "dev"
+  }
+}
+
 output aws_vpc_id {
-  value       = aws_vpc.example
+  value       = aws_vpc.example.id
   sensitive   = true
   description = "ID of the above VPC"
 }
